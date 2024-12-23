35b7841a209bfce46e3df41c136a46d226756d59
noseunghyeon <ro981009@gmail.com>
2024-12-23 17:42:42 +0900

    cahtbot action

--- src/components/chatbot/config.js
+++ src/components/chatbot/config.js
@@ -1,30 +1,23 @@
 import { createChatBotMessage } from "react-chatbot-kit";
-import logo_w from "../../assets/logo_w.png";
+import LoadingDots from "./LoadingDots";
 
 const config = {
   initialMessages: [createChatBotMessage("안녕하세요! 무엇을 도와드릴까요?")],
-  botName: "Chatbot",
+  // 기본 로딩 위젯 비활성화
+  widgets: [],
   customComponents: {
-    header: () => (
-      <div className="react-chatbot-kit-chat-header">유산지기 AI 챗봇</div>
-    ),
-    botAvatar: () => (
-      <img
-        src={logo_w}
-        alt="챗봇 아바타"
-        className="react-chatbot-kit-chat-bot-avatar bg-blue-900 "
-      />
-    ),
+    // LoadingDots 컴포넌트를 등록
+    loading: (props) => <LoadingDots {...props} />,
   },
   customStyles: {
+    // 기본 로딩 스타일 제거
+    loadingContainer: {
+      display: "none",
+    },
     botMessageBox: {
       backgroundColor: "#376B7E",
     },
     chatButton: {
-      backgroundColor: "#376B7E",
-    },
-    loadingDots: {
-      color: "#376B7E",
       backgroundColor: "#376B7E",
     },
   },
