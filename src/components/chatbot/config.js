8ca731c6b81b4d7d6525cb3ea26a144aec69f9ce
noseunghyeon <ro981009@gmail.com>
2024-12-23 17:55:35 +0900

    config

--- src/components/chatbot/config.js
+++ src/components/chatbot/config.js
@@ -1,26 +1,31 @@
 import { createChatBotMessage } from "react-chatbot-kit";
-import LoadingDots from "./LoadingDots";
-
+import logo_w from "../../assets/logo_w.png";
 const config = {
   initialMessages: [createChatBotMessage("안녕하세요! 무엇을 도와드릴까요?")],
-  // 기본 로딩 위젯 비활성화
-  widgets: [],
+  botName: "Chatbot",
   customComponents: {
-    // LoadingDots 컴포넌트를 등록
-    loading: (props) => <LoadingDots {...props} />,
+    header: () => (
+      <div className="react-chatbot-kit-chat-header">유산지기 AI 챗봇</div>
+    ),
+    botAvatar: () => (
+      <img
+        src={logo_w}
+        alt="챗봇 아바타"
+        className="react-chatbot-kit-chat-bot-avatar bg-blue-900 "
+      />
+    ),
   },
   customStyles: {
-    // 기본 로딩 스타일 제거
-    loadingContainer: {
-      display: "none",
-    },
     botMessageBox: {
       backgroundColor: "#376B7E",
     },
     chatButton: {
       backgroundColor: "#376B7E",
     },
+    loadingDots: {
+      color: "#376B7E",
+      backgroundColor: "#376B7E",
+    },
   },
 };
-
 export default config;
